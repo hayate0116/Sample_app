e50be19d0de1862d3e63020b7b37ba77f5829adc
hayate0116 <83390204+hayate0116@users.noreply.github.com>
2023-05-08 01:39:47 +0000

    [Finish]2-flash

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
  def create
    list = List.new(list_params)
    list.save
+   flash[:notice] = "投稿が成功しました"
    redirect_to list_path(list.id)
  end
 
@@ -18,11 +19,33 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end  
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
